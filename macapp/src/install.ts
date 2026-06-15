2483a53ae021d5583a2b1510ed08ac9820171fc0
Allen <era.rawk@gmail.com>
2026-06-15 17:03:02 +0800

    Fix downloader app startup crash

--- macapp/src/install.ts
+++ macapp/src/install.ts
@@ -1,22 +1,33 @@
 import * as fs from 'fs'
 import { exec as cbExec } from 'child_process'
 import { app as electronApp } from 'electron'
-import { app as remoteApp } from '@electron/remote'
 import * as path from 'path'
 import { promisify } from 'util'
 
-const app = process && process.type === 'renderer' ? remoteApp : electronApp
-const ollama = app.isPackaged
-  ? path.join(process.resourcesPath, 'darwin', 'ollama')
-  : path.resolve(process.cwd(), '..', 'dist', 'darwin', 'ollama')
 const exec = promisify(cbExec)
 const symlinkPath = '/usr/local/bin/ollama'
 
+function isPackaged() {
+  if (process && process.type === 'renderer') {
+    return !(process as NodeJS.Process & { defaultApp?: boolean }).defaultApp
+  }
+
+  return electronApp.isPackaged
+}
+
+function ollamaPath() {
+  return isPackaged()
+    ? path.join(process.resourcesPath, 'darwin', 'ollama')
+    : path.resolve(process.cwd(), '..', 'dist', 'darwin', 'ollama')
+}
+
 export function installed() {
+  const ollama = ollamaPath()
   return fs.existsSync(symlinkPath) && fs.readlinkSync(symlinkPath) === ollama
 }
 
 export async function install() {
+  const ollama = ollamaPath()
   const command = `do shell script "mkdir -p ${path.dirname(
     symlinkPath
   )} && ln -F -s \\"${ollama}\\" \\"${symlinkPath}\\"" with administrator privileges`
